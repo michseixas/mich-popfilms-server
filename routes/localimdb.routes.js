63a71070ff75ebd023fbd6f762682c0c0aa829a3
Mich Seixas <mcseixas@gmail.com>
2023-10-12 18:23:11 +0200

    created mostpopularmovies database on MongoDB. Created model and routes for mostpopular

--- routes/localimdb.routes.js
+++ routes/localimdb.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Top250 = require("../models/Top250.model");
+const MostPopular = require("../models/MostPopular.model");
 
 router.get("/Top250Movies", (req, res, next) => {
   Top250.find({})
@@ -9,7 +10,18 @@ router.get("/Top250Movies", (req, res, next) => {
     })
     .catch((err) => {
       console.log(err);
-      res.status(500).json({ error: "Failed to retrieve movies" });
+      res.status(500).json({ error: "Failed to retrieve top 250 movies" });
+    });
+});
+
+router.get("/MostPopularMovies", (req, res, next) => {
+  MostPopular.find({})
+    .then((items) => {
+      res.send({ items });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: "Failed to retrieve most popular movies" });
     });
 });
 
