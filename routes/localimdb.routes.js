b07d9ee920f3e3f3a21cf2feb1d648519d4fcca6
Mich Seixas <mcseixas@gmail.com>
2023-10-12 16:30:26 +0200

    change the result of the query to an array named items instead of top250

--- routes/localimdb.routes.js
+++ routes/localimdb.routes.js
@@ -4,8 +4,8 @@ const Top250 = require("../models/Top250.model");
 
 router.get("/Top250Movies", (req, res, next) => {
   Top250.find({})
-    .then((top250) => {
-      res.send({ top250 });
+    .then((items) => {
+      res.send({ items });
     })
     .catch((err) => {
       console.log(err);
